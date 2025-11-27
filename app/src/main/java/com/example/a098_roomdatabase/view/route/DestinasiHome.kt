c2f9d60fef0ab925ba5fac951161dc69ed401155
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2025-11-27 17:13:30 +0700

    merepresentasikan destinasi (halaman) Home

---
+++ app/src/main/java/com/example/a098_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.a098_roomdatabase.view.route
+
+import com.example.a098_roomdatabase.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
